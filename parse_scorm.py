a5566b353d86a6399ec41c13c60449ca346c47b3
“Jimbob12505” <vatsal.12505@gmail.com>
2024-07-16 18:59:24 +0530

    Fixed mistake with method reference

--- parse_scorm.py
+++ parse_scorm.py
@@ -5,5 +5,3 @@ def extract_scorm_package(zip_path, extract_to):
     with zipfile.ZipFile(zip_path, 'r') as zip_ref:
         zip_ref.extractall(extract_to)
 
-
-extract_scorm_package("source/Effective Communication Skills.zip", "output")
